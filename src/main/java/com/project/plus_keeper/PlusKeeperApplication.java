c5d3dca52e350a3907dcdd47655ef208e8b33769
gyeyeon <iam.gyeyeon@gmail.com>
2024-06-11 21:50:03 +0900

    init spring boot project

---
+++ src/main/java/com/project/plus_keeper/PlusKeeperApplication.java
@@ -0,0 +1,13 @@
+package com.project.plus_keeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlusKeeperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlusKeeperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/plus_keeper/PlusKeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.plus_keeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlusKeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
