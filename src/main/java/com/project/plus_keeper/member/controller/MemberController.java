5b0cef6f1cf1b08c3e802b9b2bd57ba584f26e98
gyeyeon <iam.gyeyeon@gmail.com>
2024-06-21 15:36:37 +0900

    @ResponseBody 태그 누락으로 인해 타임리프로 연결되는 현상 수정

--- src/main/java/com/project/plus_keeper/member/controller/MemberController.java
+++ src/main/java/com/project/plus_keeper/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import com.project.plus_keeper.member.services.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -19,9 +20,9 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-
+    @ResponseBody
     @PostMapping("/api/members")
-    public List<Member> list(Member member) {
+    public List<Member> list() {
         return memberService.findAllMembers();
     }
 }
